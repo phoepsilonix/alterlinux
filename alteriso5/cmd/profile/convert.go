504fa0bcfcf7d8d746796c1886ccefb2cf5a56c3
hayao <shun819.mail@gmail.com>
2024-08-17 17:38:03 +0900

    Move: profile dir

---
+++ alteriso5/cmd/profile/convert.go
@@ -0,0 +1,25 @@
+package profile
+
+import (
+	"errors"
+
+	"github.com/spf13/cobra"
+)
+
+func convertCmd() *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "convert profile out",
+		Short: "Convert profile for archiso",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return errors.New("not implemented")
+		},
+	}
+
+	return &cmd
+
+}
+
+func init() {
+	subCmds.RegisterSubCmd(convertCmd())
+}
